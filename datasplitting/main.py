ed4fd21a40c45a3542ba04aebf20cee76884bb28
ml-utils <machinelearningutils@gmail.com>
2021-11-18 02:38:41 +0100

    todo notes

--- datasplitting/main.py
+++ datasplitting/main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     folds = DataSplits.get_random_k_folds(k, inX, outy, dataset_config)
     # todo: only use the training folds for the correlation matrix
     # todo: use merged folds for the training set
+    # todo: use merged folds to do univariate analysis of features
 
     # FeaturesAnalysis.plot_correlation_matrix2(inX, outy, dataset_config)
     FeaturesAnalysis.plot_densities(inX, outy, dataset_config)
@@ -22,3 +23,27 @@ if __name__ == '__main__':
 
     # todo, draw some learning curves (regression, scikit library nn, ..)
     # todo gridsearch cross validation
+
+    #todo:
+    # activation functions: add relu
+    # feature engineering: add x^2 for each numerical input variable
+
+    # steps (try first on a similar dataset for regression, like the airfoil dataset):
+
+    # data split (CV design)
+    # feature angineering: linear base expansion (add x^2 and possibly others)
+    # feature analysis (ie correlation, distribution) and possibly selection
+    # grid search for model selection (dev set)
+    ## possible hyperparameters to select:
+    ## n. of NN nodes/units and layers
+    ## eta, gamma, lambda (learining rate, momentum, regularization)
+    ## activation functions (relu, tanh)
+    ## loss functions (L2 norm, L1 norm, ..)
+    # evaluation (test set)
+    # last model selection
+    # last training on whole dataset, with the model selected at the previous step
+
+    # possible models to compare:
+    # NN with random weights and no training
+    # some librariy NN
+    # ..
