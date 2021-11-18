dad5ba6c4cb7e4e36aad3cd82ed06c5ac2010b53
ml-utils <machinelearningutils@gmail.com>
2021-11-18 20:50:24 +0100

    added base for xor test

--- unit_tests/test_nn.py
+++ unit_tests/test_nn.py
@@ -7,7 +7,7 @@ from nn import neuralnet
 
 
 class UnitTestsNeuralNet(unittest.TestCase):
-    def test_init(self):
+    def test_pattern_update(self):
         nn1 = neuralnet(units=(50, ), activation='tanh')
         config = DataSplits.get_dataset_configuration('airfoil')
         _, inX, outy = DataSplits.load_the_dataset(config, otherpath='../datasplitting/assets/airfoil/')
@@ -15,6 +15,15 @@ class UnitTestsNeuralNet(unittest.TestCase):
             idx = 0
             label = outy[idx]
             nn1.pattern_update(sample, label)
+
+    def test_xor_numeric(self):
+        nn1 = neuralnet(units=(4,), activation='tanh')
+        # samples: x1, x2, y =  000 011 101 110
+        samples = [[0,0,0], [0,1,1], [1,0,1], [1,1,0]]
+        for sample in samples:
+            input = numpy.array(sample[0:2])
+            label = numpy.array(sample[2])
+            nn1.pattern_update(input, label)
 
 
 if __name__ == '__main__':
